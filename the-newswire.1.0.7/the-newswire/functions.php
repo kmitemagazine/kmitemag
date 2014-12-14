c1b57fd31359f339d7801cba012c660388b26d5f
Arjun Munji <arjunmunji@hotmail.com>
2014-12-14 22:10:05 +0530

    added functions.php in newswire child theme and fixed author issue

--- the-newswire.1.0.7/the-newswire/functions.php
+++ the-newswire.1.0.7/the-newswire/functions.php
@@ -912,3 +912,12 @@ endif;
 add_action('wp_enqueue_scripts', 'newswire_custom_scripts');
 
 ?>
+<?php
+
+function issuem_post_author_archive( &$query )
+{
+    if ( $query->is_author )
+        $query->set( 'post_type', 'article' );
+    remove_action( 'pre_get_posts', 'issuem_post_author_archive' ); // run once!
+}
+add_action( 'pre_get_posts', 'issuem_post_author_archive' );
