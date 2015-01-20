44a0af5e869cc233c9cba1f7cf8a6f52b2d76d0e
Arjun Munji <arjunmunji@hotmail.com>
2015-01-20 23:25:28 +0530

    new logo ideas

--- the-newswire.1.0.7-child/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function issuem_post_author_archive( &$query )
-{
-    if ( $query->is_author )
-        $query->set( 'post_type', 'article' );
-    remove_action( 'pre_get_posts', 'issuem_post_author_archive' ); // run once!
-}
-add_action( 'pre_get_posts', 'issuem_post_author_archive' );
